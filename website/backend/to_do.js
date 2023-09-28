9c8406dec72b78ae0bb257efff187d1c3aaf8ebf
floetenheini <fischerjonas@mediamarktsaturn.de>
2023-09-28 15:34:31 +0200

    first working version of the To-Do app

--- website/backend/to_do.js
+++ website/backend/to_do.js
@@ -1,9 +1,27 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todos = document.getElementById("todos");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const todo = input.nodeValue;
-  if (todo) {
+
+  const todoText = input.value;
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () =>
+      todoEl.classList.toggle("completed")
+    );
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+    });
+
+    todos.appendChild(todoEl);
+
+    input.value = "";
   }
 });
