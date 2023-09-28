80e7e4a5e81b5323708bd35a9e0fc4f8690fa84f
floetenheini <fischerjonas@mediamarktsaturn.de>
2023-09-28 17:20:18 +0200

    fancy background and first steps to login process

--- website/backend/index.js
+++ website/backend/index.js
@@ -7,3 +7,27 @@ function onButtonClick() {
 
 const button = document.querySelector("button");
 button.addEventListener("click", onButtonClick);
+
+const input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todoText = input.value;
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () =>
+      todoEl.classList.toggle("completed")
+    );
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+    });
+
+    input.value = "";
+  }
+});
